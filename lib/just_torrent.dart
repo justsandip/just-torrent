248a3a1ff1a26971301edc7c93c298c0f8bcc6ee
Sandip <sandipramanik07@gmail.com>
2026-03-05 22:47:03 +0530

    initialize repository ✨

---
+++ lib/just_torrent.dart
@@ -0,0 +1,10 @@
+/// just_torrent, Yet another BitTorrent CLI client, just built with Dart
+///
+/// ```sh
+/// # activate just_torrent
+/// dart pub global activate just_torrent
+///
+/// # see usage
+/// just_torrent --help
+/// ```
+library;
